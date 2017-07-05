e3eaa7b25c9cd2c65f537852a461c1ca760f9068
TeBeCo <tebeco@gmail.com>
2017-07-05 15:21:54 +0200

    feat(setup): Docker Linux+ windows make an http call to the Linux one

---
+++ src/WinDocker/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WinDocker.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            string result;
+            try
+            {
+                var httpClient = new HttpClient();
+                result = await httpClient.GetStringAsync("http://linuxdocker/api/values");
+            }
+            catch (Exception ex)
+            {
+                result = ex.Message;
+            }
+            return Json(new { FromLinux = result });
+        }
+    }
+}
